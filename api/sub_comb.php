7196e821f250a7eec31512aceb20c2e91893ecab
outcastveron <39554024+outcastveron@users.noreply.github.com>
2024-03-14 16:27:59 +0800

    Add files via upload

---
+++ api/sub_comb.php
@@ -0,0 +1,49 @@
+<?php
+
+ini_set('date.timezone','Asia/Shanghai');
+set_time_limit(300);
+
+// 订阅链接的列表
+$subUrlList = [
+  'https://v2board.tech/api/v1/client/subscribe?token=11111',
+  'https://v1.efshop.cc/api/v1/client/subscribe?token=22222',
+  'https://v2board.tech/api/v1/client/subscribe?token=33333',
+];
+
+try {
+  $allList = [];
+
+  foreach ($subUrlList as $url) {
+    $subString = fetchContent($url);
+    $subContent = base64_decode(trim($subString));
+
+    array_push($allList, $subContent);
+  }
+
+  $result = implode("\n", $allList);
+  $result = base64_encode($result);
+
+  echo $result;
+} catch (Exception $e) {
+  var_dump($e->getMessage());
+}
+
+function fetchContent($url) {
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, 0);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+  $rs = curl_exec($ch);
+
+  curl_close($ch);
+
+  return $rs;
+}
