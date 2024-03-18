701240261586b8332f54b8de33b22f29eaa8e2ae
outcastveron <39554024+outcastveron@users.noreply.github.com>
2024-03-19 07:13:47 +0800

    Update sub.php

--- api/sub.php
+++ api/sub.php
@@ -5,11 +5,7 @@ set_time_limit(300);
 
 // 订阅链接的列表
 $subUrlList = [
-  'https://raw.githubusercontent.com/txmiaomiaowu/openitr/main/long',
-  'https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/Eternity',
-  'https://raw.githubusercontent.com/obscure1990/freeVM/master/list.txt',
-  'https://raw.githubusercontent.com/wudongdefeng/free/main/freevm/list.txt',
-  'https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/Long_term_subscription_num',
+  'https://sub.tgzdyz2.xyz/sub',
   'https://sub.5112233.xyz/auto',
   'https://sub3.jie-quick.buzz/api/v1/client/subscribe?token=4922263ac084a8daa39c93b38c3772fc',
 ];
