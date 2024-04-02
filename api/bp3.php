612b448186379f16fc8109aa6afa33d3723a8bcf
outcastveron <39554024+outcastveron@users.noreply.github.com>
2024-04-02 08:45:28 +0800

    Create bp3.php

---
+++ api/bp3.php
@@ -0,0 +1,49 @@
+<?php
+
+ini_set('date.timezone','Asia/Shanghai');
+set_time_limit(300);
+
+// 订阅链接的列表
+$subUrlList = [
+  'https://cm8.rr.nu/lucky', 
+  'https://vless.fxxk.dedyn.io/auto',
+  'https://vmess.fxxk.dedyn.io/auto',
+  ];
+
+try {
+  $allList = [];
+
+  foreach ($subUrlList as $url) {
+    $subString = fetchContent($url);
+    $subContent = base64_decode(trim($subString));
+
+    array_push($allList, $subContent);
+  }
+
+  $result = implode("\n", $allList);
+  $result = base64_encode($result);
+
+  echo $result;
+} catch (Exception $e) {
+  var_dump($e->getMessage());
+}
+
+function fetchContent($url) {
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, 0);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+  $rs = curl_exec($ch);
+
+  curl_close($ch);
+
+  return $rs;
+}
