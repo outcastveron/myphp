23f1c39b96e07f57feeb585e9711363dbf4d6b75
outcastveron <39554024+outcastveron@users.noreply.github.com>
2024-07-16 08:34:31 +0800

    Update bp.php

--- api/bp.php
+++ api/bp.php
@@ -6,10 +6,11 @@ set_time_limit(300);
 // 订阅链接的列表
 $subUrlList = [
   'https://zero.76898102.xyz/link/rWdgl3mSG2hklCsG?sub=3',
-  'https://xn--rssy0da8807a.cc/api/v1/client/subscribe?token=e454ad5e18859d2a093b78d94c3f9b37',
-  'https://sub12316.tnetvpn.site/api/v1/client/subscribe?token=e6f86acae267cf3ce87fba50d639509f',
-  'https://sub46299.yljc.online/api/v1/client/subscribe?token=5320b3bb55386c9a003de157df3839a5',
+  'http://panda.切底锚栓.com/api/v1/client/subscribe?token=3a3a14f2d494670a29b9bc61bff9d58d',
+  'https://chaozhijc.xyz/api/v1/client/subscribe?token=3c2d11bd3aa96353289b6c873040f32d',
+  'https://dash.djjc.cfd/api/v1/client/subscribe?token=be34b72308d6befd5867061788ed0f21',
   'https://rvps.duckking.shop:26720/s/default/764ed3a1a918ee05994ba37a1340ca29',
+  'https://id9.cc/sub?target=v2ray&url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fmjjjd%2Flinuxdo%40main%2Fdata%2F2024_07_10%2FdswnxV.yaml&insert=false&emoji=true&list=false&tfo=false&scv=false&fdn=false&sort=false',
 ];
 
 try {
