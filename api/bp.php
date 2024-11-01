b44d6dec8c9b38009ff9d0f1934f074892ac9df7
outcastveron <39554024+outcastveron@users.noreply.github.com>
2024-11-01 08:35:51 +0800

    Update bp.php

--- api/bp.php
+++ api/bp.php
@@ -7,7 +7,7 @@ set_time_limit(300);
 $subUrlList = [
   'https://sub.ztyawc.us.kg/zzzjp',
   'https://id9.cc/sub?target=v2ray&url=https%3A%2F%2Fraw.githubusercontent.com%2Fanaer%2FSub%2Fmain%2Fclash.yaml&insert=false&emoji=true&list=false&tfo=false&scv=false&fdn=false&sort=false',
-  'https://rvps.duckking.shop:26720/s/default/764ed3a1a918ee05994ba37a1340ca29',
+  'https://app.jisovpn.site/api/v1/client/subscribe?token=fae9ac4bac4624f9e2eca15293b953f6',
   'http://106.75.178.88:82/api/v1/client/subscribe?token=3054da66436dba0c9f66cfbcb79c7199',
   'https://selinux.link/api/v1/client/subscribe?token=de9706e1-7b34-4420-908d-de94c5fac34f',
   'https://v2b.hinetlove.site/api/v1/client/subscribe?token=0f51ebe437aad648d7ca71b3a6ff35a7',
